3002afc9291ce483b008741a5e69a5413aebe111
Naoto <naoto3754@gmail.com>
2016-05-29 22:00:53 +0900

    色替えゲーム作成

---
+++ Assets/Sasaki/Scripts/ColorSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSwitcher : MonoBehaviour 
+{
+	void Awake () {
+		bool isRed = this.gameObject.layer == LayerMask.NameToLayer ("Red");
+		GetComponent<Renderer>().material.color = isRed ? Color.red : Color.blue;
+	}
+	
+	void Update () {
+		if (InputManager.I.GetAnyTapUp ()) {
+			bool isRed = this.gameObject.layer == LayerMask.NameToLayer ("Red");
+			int layer = isRed ? LayerMask.NameToLayer ("Blue") : LayerMask.NameToLayer ("Red");
+			this.gameObject.layer = layer;
+			GetComponent<Renderer>().material.color = isRed ? Color.blue : Color.red;
+		}
+	}
+}
